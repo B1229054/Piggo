0bcf8874083209dfeb9e8538f4af2248ff545d53
B1229054 <145407828+B1229054@users.noreply.github.com>
2026-04-19 23:15:06 +0800

    Update quiz.tsx

--- quiz.tsx
+++ quiz.tsx
@@ -1,6 +1,4 @@
-// app/quiz.tsx
 import React from 'react';
-// 確保路徑是指向 components 資料夾
 import PersonalityQuizScreen from '../components/PersonalityQuizScreen';
 
 export default function QuizRoute() {
