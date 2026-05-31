408b190ddf48b175c8e044c2fac633213b113f9f
B1229005 <144075553+B1229005@users.noreply.github.com>
2026-06-01 02:08:32 +0800

    Delete piggo-app directory

--- piggo-app/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin', // 👈 這是為了讓拖曳動畫能運作的關鍵
-    ],
-  };
-};
