b1ca86140f9470c0805dace30b321219acc6d329
B1229005 <144075553+B1229005@users.noreply.github.com>
2026-06-01 02:05:34 +0800

    Delete piggo-app/app directory

--- piggo-app/app/quiz.tsx
+++
@@ -1,8 +0,0 @@
-// app/quiz.tsx
-import React from 'react';
-// 確保路徑是指向 components 資料夾
-import PersonalityQuizScreen from '../components/PersonalityQuizScreen';
-
-export default function QuizRoute() {
-  return <PersonalityQuizScreen />;
-}
